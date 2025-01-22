91d3a9c0b29c7807968a688e1cc64fd7b21a3c2c
Erik Torres <erik.torres@lifullconnect.com>
2025-01-22 15:20:08 +0100

    :white_check_mark: Should load application parameters

--- modules/clothing/clothing-service/src/test/scala/clothing/application/ClothingServiceParamsSuite.scala
+++ modules/clothing/clothing-service/src/test/scala/clothing/application/ClothingServiceParamsSuite.scala
@@ -1,8 +1,19 @@
 package es.eriktorr
 package clothing.application
 
+import cats.implicits.catsSyntaxEitherId
+import com.monovore.decline.{Command, Help}
 import munit.FunSuite
 
 final class ClothingServiceParamsSuite extends FunSuite:
-  test("should work"):
-    fail("not implemented")
+  test("should load default parameters"):
+    assertEquals(
+      Command(name = "name", header = "header")(ClothingServiceParams.opts).parse(List.empty),
+      ClothingServiceParams(false).asRight[Help],
+    )
+
+  test("should load parameters from application arguments"):
+    assertEquals(
+      Command(name = "name", header = "header")(ClothingServiceParams.opts).parse(List("-v")),
+      ClothingServiceParams(true).asRight[Help],
+    )
